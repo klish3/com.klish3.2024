5572d2345542ecb34bc61266da66bb4b81e6fb41
Tawanda Kanyangarara <klish3@gmail.com>
2024-08-14 14:31:50 +0100

    chore: remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Landing } from "./pages/Resume";
 import { ComingSoon } from "./pages/Resume/ComingSoon";
 
 const App = () => {
   return (
     <>
       <ComingSoon />
-      {/* <Landing /> */}
     </>
   );
 };
